765d6a7807a568125179e5ac3d2c51b84bedfa74
Rhys Mainwaring <rhys.mainwaring@me.com>
2022-09-04 23:52:46 +0100

    ros2: convert package to build with colcon / ament_cmake
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- src/example-misc.cc
+++ src/example-misc.cc
@@ -488,8 +488,8 @@ int main(int argc, char **argv) {
     if(argc < 2) {
         printf("Usage: %s <demo_name>\n", argv[0]);
         printf("Choose one of the following demos:\n");
-        typedef std::pair<std::string, void (*)(void)> demo_pair;
-        for(const demo_pair &pair : demos) {
+
+        for(auto &pair : demos) {
             printf("    %s\n", pair.first.c_str());
         }
         return 0;
